bf74bb093e0a927a621cdcad49d4edd8aa9404c4
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2024-09-19 16:21:37 -0500

    fix server problem

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const usersRoutes = require('./src/routes/api/users_routes');
 const reservationsRoutes = require('./src/routes/api/reservations_routes');
-const financeRoutes = require('./src/routes/api/finances_routes')
-const storeRoutes= require('./src/routes/api/store_routes')
-// const notification = require('./helpers/twilio_route')
-const slot = require('./src/routes/api/quotaLimits_routes')
+const financeRoutes = require('./src/routes/api/finances_routes');
+const storeRoutes = require('./src/routes/api/store_routes');
+// const notification = require('./helpers/twilio_route');
+const slot = require('./src/routes/api/quotaLimits_routes');
 const cors = require('cors');
 const termsAndConditionsRoutes = require('./src/routes/api/termsAndConditions_routes');
-const pqrs = require('./src/routes/api/pqrs_routes')
-
+const pqrs = require('./src/routes/api/pqrs_routes');
 
 dotenv.config();
 
@@ -28,7 +27,24 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Conexión a la base de datos exitosa'));
 
-app.use(cors());
+
+const allowedOrigins = ['https://bullfit-app-v2-0.vercel.app']; 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200, 
+};
+
+app.use(cors(corsOptions)); 
+
+// Rutas de la API
 app.use('/api', usersRoutes);
 app.use('/api', reservationsRoutes);
 app.use('/api', financeRoutes);
@@ -36,10 +52,9 @@ app.use('/api', storeRoutes);
 app.use('/api', termsAndConditionsRoutes);
 // app.use('/api', notification);
 app.use('/api', slot);
-app.use('/api',pqrs);
+app.use('/api', pqrs);
 
-
-
+// Iniciar el servidor
 app.listen(PORT, () => {
-  console.log('Servidor Express en ejecución en el puerto' + PORT);
+  console.log('Servidor Express en ejecución en el puerto ' + PORT);
 });
