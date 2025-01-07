e2c2b6af072f83836be6cf03e91ccbe36e8f406a
SantiagoMurilloV <128425308+SantiagoMurilloV@users.noreply.github.com>
2025-01-07 16:59:34 -0500

    change a bot route and bullfit token

--- src/controllers/reservations_controllers.js
+++ src/controllers/reservations_controllers.js
@@ -8,11 +8,11 @@ const UserFinance = require('../models/finances');
 const mongoose = require('mongoose');
 const moment = require('moment');
 const TelegramBot = require('node-telegram-bot-api');
-const TELEGRAM_TOKEN = '8104626358:AAHjNVWdZuY412ngB5EX47ZaxFBH8xip9NY';
-// const TELEGRAM_TOKEN = '7409507098:AAEJ_Nb1tFXcKmRExxrTaYUD6j_ntLjjAaI';
+// const TELEGRAM_TOKEN = '8104626358:AAHjNVWdZuY412ngB5EX47ZaxFBH8xip9NY';
+const TELEGRAM_TOKEN = '7409507098:AAEJ_Nb1tFXcKmRExxrTaYUD6j_ntLjjAaI'; // Bullbot
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
-// const ADMIN_CHAT_ID = '6558646628';
-const ADMIN_CHAT_ID = '2067829989';
+const ADMIN_CHAT_ID = '6558646628'; 
+
 
 
 exports.getAllReservations = async (req, res) => {
